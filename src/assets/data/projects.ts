3ae255c2a70727237e770cfa973565e315f4c1b8
Niesmer <antoine.ferron.pro@pm.me>
2025-01-07 10:45:38 +0100

    Trying image integrations for my projects

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -1,89 +1,16 @@
-// export interface ProjectInterface {
-//   titre?: string;
-//   tags?: string[];
-//   objectif?: string;
-//   description?: string;
-//   LongerDescription?: string;
-//   img?: { src: string; alt?: string }[];
-//   vid?: {
-//     vidsrc?: string;
-//     viddesc?: string;
-//   };
-//   taches?: string[];
-//   problemes?: string;
-//   retenues?: string;
-//   link?: string;
-//   annee?: number;
-// }
-
-// export const projects: ProjectInterface[] = [
-//   {
-//     titre: "D'une Halles à l'autre",
-//     description:
-//       "Réalisé en partenariat avec Laval Patrimoine cette application est un jeux d'enigme en Réalité Virtuelle et Mobile, le but est de découvrir l'histoire des halles de Laval de manière ludique",
-//     link: "halles",
-//     annee: 2024,
-//   },
-//   {
-//     titre: "Space hokey",
-//     tags: ["Android", "Java", "Game", "Duo"],
-//     img: [
-//       { src: "/src/assets/images/Space Hokey/image-6_1080x2170.png" },
-//       { src: "/src/assets/images/Space Hokey/image-8_1080x2170.png" },
-//     ],
-//     description:
-//       "Ce projet est un jeu android lors duquel j’ai majoritairement été en charge des fonctionnalités du jeu tel que la gravité le score la détection du toucher.",
-//     link: "space-hokey",
-//     annee: 2024,
-//   },
-//   {
-//     titre: "Site e‑commerce",
-
-//     description:
-//       "Ce projet est une parodis d'amazon dans le cadre de ma formation lors de ce projet j'ai réalisé une API en Syfony et un backoffice en React, afin de gérer les produits d'une boutique. La partie boutique n'as pas été réalisé",
-//     link: "amzzon",
-//     annee: 2023,
-//   },
-//   {
-//     titre: "WebApp WYS",
-//     tags: ["Web", "React", "Stage"],
-//     img: [{ src: "/src/assets/images/Wys/WebAppv1.png" }],
-//     description:
-//       "Ce projet est réalisé lors d'un stage dans l'entreprise win your star, cette webapp est une version numérique du jeu d'intelligence emotionnelle WYS. J'ai été en charge de repenser et d'implémenter une nouvelle interface front avec Figma et React.",
-
-//     link: "wys-app",
-//     annee: 2024,
-//   },
-
-//   {
-//     titre: "Carte interactive",
-//     link: "map-iut",
-//     tags: ["Web", "JavaScript", "Leaflet", "Groupe de 4"],
-//     description:
-//       "Le projet consistait à réaliser une carte interactive regroupant chaque IUT de france la carte dispose de fonctionnalité de recherche avancé, ce projet était réalisé en groupe de 4 en méthode agile. Ce projet était en partenariat avec l'UNPIUT et l'association des directeurs d'IUT.",
-//     img: [{ src: "/src/assets/images/Carte Iut/Explorateur IUT 1.png" }],
-
-//     annee: 2023,
-//   },
-// ];
-
 export interface ProjectInterface {
   titre?: string;
   tags?: string[];
   objectif?: string;
   description?: string;
-  LongerDescription?: string;
-  img?: { src: string; alt?: string }[];
-  vid?: { vidsrc?: string; viddesc?: string };
-  taches?: string[];
-  problemes?: string;
-  retenues?: string;
+  sections?: Array<{
+    titre: string;
+    texte: string | Array<string>;
+    imgs?: Array<{ src: string; alt: string }>;
+    vids?: Array<{ src: string; alt: string }>;
+  }>;
   link?: string;
   annee?: number;
-
-  // New optional parameters
-  challenges?: string[];
-  learnings?: string[];
 }
 
 export const projects: ProjectInterface[] = [
@@ -94,31 +21,39 @@ export const projects: ProjectInterface[] = [
     link: "halles",
     annee: 2024,
     tags: ["VR", "Unity", "Mobile", "C#"],
-    taches: [
-      "Modélisation des assets 3D nécessaires au jeu.",
-      "Développement de la logique pour les interactions VR et mobiles.",
-      "Refactorisation du code pour unifier les deux environnements (VR et mobile).",
-    ],
-    challenges: [
-      "Reprendre et restructurer le code existant pour assurer la compatibilité.",
-      "Intégrer efficacement les environnements VR et mobile.",
-      "Apprendre Unity et C# rapidement pour répondre aux besoins du projet.",
-    ],
-    retenues:
-      "Apprentissage intensif d'Unity et C#, ainsi qu'une meilleure compréhension des contraintes liées aux projets cross-platform.",
-    learnings: [
-      "Maîtrise des concepts fondamentaux de Unity et C#.",
-      "Amélioration des compétences en refactorisation et optimisation du code.",
-      "Meilleure gestion des projets collaboratifs complexes.",
-    ],
-    img: [
+    sections: [
       {
-        src: "/src/assets/images/halles/vr_session.png",
-        alt: "Session VR dans le jeu 'D'une Halles à l'autre'",
+        titre: "Tâches",
+        texte: [
+          "Modélisation des assets 3D nécessaires au jeu.",
+          "Développement de la logique pour les interactions VR et mobiles.",
+          "Refactorisation du code pour unifier les deux environnements (VR et mobile)."
+        ],
+        imgs: [
+          {
+            src: "/src/assets/images/halles/vr_session.webp",
+            alt: "Session VR dans le jeu 'D'une Halles à l'autre'",
+          },
+          {
+            src: "/src/assets/images/halles/3d_model.webp",
+            alt: "Modélisation 3D des Halles",
+          },
+        ],
       },
       {
-        src: "/src/assets/images/halles/3d_model.png",
-        alt: "Modélisation 3D des Halles",
+        titre: "Challenges",
+        texte:
+          "Reprendre et restructurer le code existant pour assurer la compatibilité. Intégrer efficacement les environnements VR et mobile. Apprendre Unity et C# rapidement pour répondre aux besoins du projet.",
+      },
+      {
+        titre: "Retenues",
+        texte:
+          "Apprentissage intensif d'Unity et C#, ainsi qu'une meilleure compréhension des contraintes liées aux projets cross-platform.",
+      },
+      {
+        titre: "Learnings",
+        texte:
+          "Maîtrise des concepts fondamentaux de Unity et C#. Amélioration des compétences en refactorisation et optimisation du code.",
       },
     ],
   },
@@ -128,30 +63,40 @@ export const projects: ProjectInterface[] = [
       "Un jeu Android de air hockey au thème spatial réalisé en Java.",
     link: "space-hokey",
     annee: 2024,
-    tags: ["Android", "Java", "Game", "Physics"],
-    taches: [
-      "Création du système de collisions pour le palet et les bords.",
-      "Développement du suivi des scores et de la logique de jeu.",
-      "Tentative d'implémentation d'un système basé sur la vélocité de la poignée.",
-    ],
-    challenges: [
-      "Gérer les limitations de temps pour finaliser toutes les fonctionnalités prévues.",
-      "Créer un moteur de collision réaliste adapté au gameplay.",
-    ],
-    retenues:
-      "Approfondissement des principes de physique appliqués au développement de jeux.",
-    learnings: [
-      "Meilleure gestion de la détection de collisions.",
-      "Optimisation de la logique pour une expérience fluide.",
-    ],
-    img: [
+    tags: ["Android", "Java", "Game"],
+    sections: [
       {
-        src: "/src/assets/images/space-hokey/hokey_table.png",
-        alt: "Table de jeu de Space Hockey",
+        titre: "Tâches",
+        texte: [
+          "Création du système de collisions pour le palet et les bords.",
+          "Développement du suivi des scores et de la logique de jeu.",
+          "Tentative d'implémentation d'un système basé sur la vélocité de la poignée."
+        ],
+        imgs: [
+          {
+            src: "/src/assets/images/space-hokey/acceuil.webp",
+            alt: "Table de jeu de Space Hockey",
+          },
+          {
+            src: "/src/assets/images/space-hokey/gameplay.webp",
+            alt: "Gameplay de Space Hockey avec thème spatial",
+          },
+        ],
       },
       {
-        src: "/src/assets/images/space-hokey/gameplay.png",
-        alt: "Gameplay de Space Hockey avec thème spatial",
+        titre: "Challenges",
+        texte:
+          "Gérer les limitations de temps pour finaliser toutes les fonctionnalités prévues. Créer un moteur de collision réaliste adapté au gameplay.",
+      },
+      {
+        titre: "Retenues",
+        texte:
+          "Approfondissement des principes de physique appliqués au développement de jeux.",
+      },
+      {
+        titre: "Learnings",
+        texte:
+          "Meilleure gestion de la détection de collisions. Optimisation de la logique pour une expérience fluide.",
       },
     ],
   },
@@ -162,29 +107,39 @@ export const projects: ProjectInterface[] = [
     link: "amzzon",
     annee: 2023,
     tags: ["Symfony", "React", "API", "Backend"],
-    taches: [
-      "Réalisation d'une API REST en Symfony pour gérer les produits.",
-      "Création d'un backoffice en React pour administrer le contenu.",
-      "Ajout de protections contre CSRF et XSS.",
-    ],
-    challenges: [
-      "Mettre en place une API robuste en respectant les bonnes pratiques.",
-      "Garantir la sécurité des données dans un environnement collaboratif.",
-    ],
-    retenues:
-      "Expérience enrichissante sur la combinaison d'outils backend et frontend pour un projet complet.",
-    learnings: [
-      "Développement d'API sécurisées.",
-      "Utilisation avancée des composants React pour des interfaces modulaires.",
-    ],
-    img: [
+    sections: [
       {
-        src: "/src/assets/images/amzzon/admin_dashboard.png",
-        alt: "Tableau de bord du backoffice",
+        titre: "Tâches",
+        texte: [
+          "Réalisation d'une API REST en Symfony pour gérer les produits.",
+          "Création d'un backoffice en React pour administrer le contenu.",
+          "Ajout de protections contre CSRF et XSS."
+        ],
+        imgs: [
+          {
+            src: "/src/assets/images/amzzon/admin_dashboard.webp",
+            alt: "Tableau de bord du backoffice",
+          },
+          {
+            src: "/src/assets/images/amzzon/api_endpoints.webp",
+            alt: "Endpoints de l'API Symfony",
+          },
+        ],
       },
       {
-        src: "/src/assets/images/amzzon/api_endpoints.png",
-        alt: "Endpoints de l'API Symfony",
+        titre: "Challenges",
+        texte:
+          "Mettre en place une API robuste en respectant les bonnes pratiques. Garantir la sécurité des données dans un environnement collaboratif.",
+      },
+      {
+        titre: "Retenues",
+        texte:
+          "Expérience enrichissante sur la combinaison d'outils backend et frontend pour un projet complet.",
+      },
+      {
+        titre: "Learnings",
+        texte:
+          "Développement d'API sécurisées. Utilisation avancée des composants React pour des interfaces modulaires.",
       },
     ],
   },
@@ -195,25 +150,39 @@ export const projects: ProjectInterface[] = [
     link: "wys-app",
     annee: 2024,
     tags: ["React", "Docker", "Figma", "Frontend"],
-    taches: [
-      "Réalisation des maquettes sur Figma.",
-      "Développement et intégration du nouveau design dans React.",
-      "Ajout d'un rôle administrateur pour une meilleure gestion.",
-    ],
-    challenges: [
-      "Reprendre un projet complexe sur Docker sans formation initiale.",
-      "Assurer la compatibilité avec l'ancien code tout en ajoutant des fonctionnalités modernes.",
-    ],
-    retenues:
-      "Un projet exigeant qui a renforcé ma capacité d'adaptation et mon expertise en développement front.",
-    learnings: [
-      "Utilisation des conteneurs Docker dans des projets web.",
-      "Création et refactorisation de rôles pour une meilleure gestion des accès.",
-    ],
-    img: [
+    sections: [
       {
-        src: "/src/assets/images/wys/interface.png",
-        alt: "Nouvelle interface utilisateur de la WebApp WYS",
+        titre: "Tâches",
+        texte: [
+          "Réalisation des maquettes sur Figma.",
+          "Développement et intégration du nouveau design dans React.",
+          "Ajout d'un rôle administrateur pour une meilleure gestion."
+        ],
+        imgs: [
+          {
+            src: "/src/assets/images/wys/old_interface.webp",
+            alt: "Ancienne interface utilisateur de la WebApp WYS",
+          },
+          {
+            src: "/src/assets/images/wys/interface.webp",
+            alt: "Nouvelle interface utilisateur de la WebApp WYS",
+          },
+        ],
+      },
+      {
+        titre: "Challenges",
+        texte:
+          "Reprendre un projet complexe sur Docker sans formation initiale. Assurer la compatibilité avec l'ancien code tout en ajoutant des fonctionnalités modernes.",
+      },
+      {
+        titre: "Retenues",
+        texte:
+          "Un projet exigeant qui a renforcé ma capacité d'adaptation et mon expertise en développement front.",
+      },
+      {
+        titre: "Learnings",
+        texte:
+          "Utilisation des conteneurs Docker dans des projets web. Création et refactorisation de rôles pour une meilleure gestion des accès.",
       },
     ],
   },
@@ -224,25 +193,35 @@ export const projects: ProjectInterface[] = [
     link: "map-iut",
     annee: 2023,
     tags: ["JavaScript", "Leaflet", "API", "Team"],
-    taches: [
-      "Analyse de la concurrence et interviews des utilisateurs cibles.",
-      "Développement du système de recherche et filtrage.",
-      "Implémentation de l'affichage des données via Echarts.js.",
-    ],
-    challenges: [
-      "Gestion de données incomplètes ou mal collectées.",
-      "Utilisation du versioning pour la première fois dans un projet d'équipe.",
-    ],
-    retenues:
-      "Une première expérience enrichissante dans un projet collaboratif impliquant plusieurs rôles.",
-    learnings: [
-      "Approfondissement des outils de visualisation comme Leaflet.",
-      "Collaboration efficace au sein d'une équipe avec Git.",
-    ],
-    img: [
+    sections: [
       {
-        src: "/src/assets/images/map-iut/interactive_map.png",
-        alt: "Interface de la carte interactive",
+        titre: "Tâches",
+        texte: [
+          "Analyse de la concurrence et interviews des utilisateurs cibles.",
+          "Développement du système de recherche et filtrage.",
+          "Implémentation de l'affichage des données via Echarts.js."
+        ],
+        imgs: [
+          {
+            src: "/src/assets/images/map-iut/interactive_map.webp",
+            alt: "Interface de la carte interactive",
+          },
+        ],
+      },
+      {
+        titre: "Challenges",
+        texte:
+          "Gestion de données incomplètes ou mal collectées. Utilisation du versioning pour la première fois dans un projet d'équipe.",
+      },
+      {
+        titre: "Retenues",
+        texte:
+          "Une première expérience enrichissante dans un projet collaboratif impliquant plusieurs rôles.",
+      },
+      {
+        titre: "Learnings",
+        texte:
+          "Approfondissement des outils de visualisation comme Leaflet. Collaboration efficace au sein d'une équipe avec Git.",
       },
     ],
   },
--- src/pages/project/[id].vue
+++ src/pages/project/[id].vue
@@ -1,68 +1,9 @@
-<!-- <script setup lang="ts">
-import { useRoute } from 'vue-router';
-import { projects } from '../../assets/data/projects';
-import Flicking from '@egjs/vue3-flicking';
-import { Perspective } from '@egjs/flicking-plugins';
-import { ref } from 'vue';
-const route = useRoute();
-const id = (route.params as { id: string }).id;
-const perspective = [new Perspective({ rotate: 0.2, perspective: 500, scale: 0.4 })];
-const projectData = projects.find((p) => p.link === id);
-const entering = ref(1);
-if (!projectData) {
-  throw new Error(`Project with link ${id} not found`);
-}
-</script>
-
-<template>
-  <div class="px-4">
-    <div class="grid justify-center pb-6">
-      <h1>{{ projectData?.titre }}</h1>
-      <div class="flex flex-wrap justify-center gap-2 ">
-        <h2 class="rounded-full py-1 px-3" v-for="tag in projectData.tags" :key="tag">
-          {{ tag }}
-        </h2>
-      </div>
-    </div>
-    <Flicking class="z-0 overflow-visible" :options="{ align: 'center', circular: true }" :plugins="perspective">
-      <div class="flicking-panel max-h-fit h-52 w-1/3 justify-center flex select-none" v-for="image in projectData?.img"
-        :key="image.src">
-        <img class="select-none object-contain" draggable="false" :src="image.src">
-      </div>
-    </Flicking>
-    <div class="description">{{ projectData?.description }}</div>
-  </div>
-</template>
-
-<style>
-h1 {
-  font-family: 'ppfragmentsans';
-  font-weight: 800;
-  text-align: center;
-  font-size: 2.5rem;
-  text-transform: uppercase;
-  margin-bottom: 15px;
-}
-
-h2 {
-  font-family: 'ppfragmentsans';
-  font-size: 1.3rem;
-  background-color: rgba(156, 156, 156, 0.6);
-  box-shadow: 0 0 2px 0px rgb(0, 0, 0);
-}
-
-.description {
-  font-family: 'ppfragmentsans';
-  font-size: 1.2rem;
-  text-align: justify;
-  margin-top: 15px;
-}
-</style> -->
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 import { projects } from '../../assets/data/projects';
 import Flicking from '@egjs/vue3-flicking';
 import { Perspective } from '@egjs/flicking-plugins';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 const route = useRoute();
 const id = (route.params as { id: string }).id;
@@ -72,8 +13,24 @@ const projectData = projects.find((p) => p.link === id);
 if (!projectData) {
   throw new Error(`Project with link ${id} not found`);
 }
-</script>
 
+// Reactive flag for large screens
+const isLargeScreen = ref(false);
+
+function updateScreenSize() {
+  isLargeScreen.value = window.innerWidth >= 1024; // Tailwind `lg` breakpoint
+}
+
+onMounted(() => {
+  updateScreenSize();
+  window.addEventListener('resize', updateScreenSize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenSize);
+});
+
+</script>
 <template>
   <div class="px-4 ppfragment overflow-hidden">
     <!-- Title -->
@@ -86,40 +43,29 @@ if (!projectData) {
       </div>
     </div>
 
-    <!-- Image Carousel -->
-    <Flicking class="z-0 overflow-visible my-2" :options="{ align: 'center', circular: true }" :plugins="perspective">
-      <div class="flicking-panel max-h-fit h-52 w-1/3 justify-center flex select-none" v-for="image in projectData.img"
-        :key="image.src">
-        <img class="select-none object-contain text-center self-center" draggable="false" :src="image.src" :alt="image.alt">
+    <!-- Image Carousel (Hidden on Large Screens) -->
+    <Flicking v-if="!isLargeScreen" class="z-0 overflow-visible my-2" :options="{ align: 'center', circular: true }"
+      :plugins="perspective">
+      <div class="flicking-panel max-h-fit h-52 w-1/3 justify-center flex select-none lg:h-fit"
+        v-for="image in projectData.img" :key="image.src">
+        <img class="select-none object-contain text-center self-center" draggable="false" :src="image.src"
+          :alt="image.alt" />
       </div>
     </Flicking>
 
     <!-- Description -->
     <div class="description">{{ projectData.description }}</div>
 
-    <!-- Tasks -->
-    <div v-if="projectData.taches && projectData.taches.length" class="mt-4">
-      <h3 class="font-bold text-lg">Missions principales :</h3>
-      <ul class="list-disc pl-6">
-        <li v-for="task in projectData.taches" :key="task">{{ task }}</li>
+    <!-- Sections -->
+    <div v-for="section in projectData.sections" :key="section.titre" class="mt-4">
+      <h3 class="font-bold text-lg">{{ section.titre }}</h3>
+      <p v-if="typeof section.texte === 'string'">{{ section.texte }}</p>
+      <ul v-else class="list-disc pl-6">
+        <li v-for="text in section.texte" :key="text">{{ text }}</li>
       </ul>
-    </div>
-
-    <!-- Challenges -->
-    <div v-if="projectData.challenges && projectData.challenges.length" class="mt-4">
-      <h3 class="font-bold text-lg">Défis rencontrés :</h3>
-      <ul class="list-disc pl-6">
-        <li v-for="challenge in projectData.challenges" :key="challenge">{{ challenge }}</li>
-      </ul>
-    </div>
-
-    <!-- Learnings -->
-    <div v-if="projectData.retenues || projectData.learnings?.length" class="mt-4">
-      <h3 class="font-bold text-lg">Ce que j'en retiens :</h3>
-      <p v-if="projectData.retenues" class="mb-2">{{ projectData.retenues }}</p>
-      <ul v-if="projectData.learnings && projectData.learnings.length" class="list-disc pl-6">
-        <li v-for="learning in projectData.learnings" :key="learning">{{ learning }}</li>
-      </ul>
+      <div v-if="section.imgs" class="flex flex-wrap justify-center gap-2 mt-2">
+        <img v-for="img in section.imgs" :key="img.src" :src="img.src" :alt="img.alt" class="w-1/3 object-contain" />
+      </div>
     </div>
   </div>
 </template>
